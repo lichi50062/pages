2ac0fc229e3b7e351f95188207e58d72c0f0ce03
6820 <lichi.li@tradevan.com.tw>
2023-02-02 09:53:56 +0800

    IE停用調整-序號1~12 無法挑選縣市別/機構代碼

--- js/BR021W.js
+++ js/BR021W.js
@@ -1,37 +1,34 @@
 ﻿//99.12.31 add 根據查詢年月.改變縣市別名稱
-function changeCity(xml, target, source, form) {
+function changeCity(target, source, form) {
       var myXML,nodeType,nodeValue, nodeName,nodeYear,m_year;      
       m_year = source.value;
       if(m_year >= 100){
          m_year = 100;
       }else{
          m_year = 99;
-      }	
-      
-      target.length = 0;      
-      var oOption;
-     
-      myXML = document.all(xml).XMLDocument;
-      nodeType = myXML.getElementsByTagName("cityType");//hsien_id
-      nodeYear = myXML.getElementsByTagName("cityYear");//m_year
-	  nodeValue = myXML.getElementsByTagName("cityValue");//hsien_id
-	  nodeName = myXML.getElementsByTagName("cityName");//hsien_name
-		
-	  oOption = document.createElement("OPTION");
-	  oOption.text='全部';
-  	  oOption.value='ALL';
-  	  target.add(oOption);
-  	  
-	  for(var i=0;i<nodeType.length ;i++)	{	  	
-  	     if (nodeYear.item(i).firstChild.nodeValue == m_year)  {
-  		    oOption = document.createElement("OPTION");
-		    oOption.text=nodeName.item(i).firstChild.nodeValue;
-  		    oOption.value=nodeValue.item(i).firstChild.nodeValue;
-  		    target.add(oOption);
-   	     }
       }
-      form.HSIEN_ID[0].selected=true;      
-      changeOption(this.document.forms[0],'');
+	var xmlDoc = $.parseXML($("xml[id=CityXML]").html()) ;
+	var data = $(xmlDoc).find("data") ;
+	target.length = 0;
+	var oOption;
+
+	oOption = document.createElement("OPTION");
+	oOption.text='全部';
+	oOption.value='ALL';
+	target.add(oOption);
+
+	$(data).each(function (i) {
+		if ($(this).find("cityyear").text() == m_year)  {
+			oOption = document.createElement("OPTION");
+			oOption.text= $(this).find("cityname").text();
+			oOption.value=$(this).find("cityvalue").text();
+			target.add(oOption);
+		}
+
+	})
+	;
+	//document.BankListfrm.HSIEN_ID[0].selected=true;//111.03.21移除不使用
+	changeOption(form,'');
 }
 
 
@@ -44,70 +41,65 @@ function changeOption(form,cnd){
        m_year = 100;
     }else{
        m_year = 99;
-    }	
-
-    myXML = document.all("TBankXML").XMLDocument;
-    form.BankListSrc.length = 0;
-    if(cnd == 'change') form.BankListDst.length = 0;
-    BnType = myXML.getElementsByTagName("BnType");    
-	nodeType = myXML.getElementsByTagName("HsienId");
-	nodeValue = myXML.getElementsByTagName("bankValue");
-	nodeName = myXML.getElementsByTagName("bankName");
-	nodeYear = myXML.getElementsByTagName("BankYear");//m_year
+    }
+	form.BankListSrc.length = 0;
+	if (cnd == 'change') {
+		form.BankListDst.length = 0;
+	}
 	var oOption;
-    var checkAdd = false;	
-	
-	for(var i=0;i<nodeType.length ;i++)
-	{
-		if(nodeYear.item(i).firstChild.nodeValue != m_year){				
-		   continue;//顯示查詢年度的新機構名稱
-		} 
+	var checkAdd = false;
+	var xmlDoc = $.parseXML($("xml[id=TBankXML]").html()) ;
+	var data = $(xmlDoc).find("data") ;
+	$(data).each(function (i) {
+		// if( form.bank_type.value!='ALL' && ($(this).find("banktype").text() != form.bank_type.value)) return;//農漁會別
+		if($(this).find("bankyear").text() != m_year)	return;//顯示查詢年度的新機構名稱111113;
 		if(form.HSIEN_ID.value == 'ALL'){
 			oOption = document.createElement("OPTION");
 			if(form.CANCEL_NO.value == 'N'){//營運中
-			   if(BnType.item(i).firstChild.nodeValue != '2'){			   	  
-			      oOption.text=nodeName.item(i).firstChild.nodeValue;
-  			      oOption.value=nodeValue.item(i).firstChild.nodeValue;   	  
-			   }		
-		    }else{//已裁撤
-		       if(BnType.item(i).firstChild.nodeValue == '2'){
-			      oOption.text=nodeName.item(i).firstChild.nodeValue;
-  			      oOption.value=nodeValue.item(i).firstChild.nodeValue;   	  
-			   }
-		    }	
-  			checkAdd=false;
-			for(var k =0;k<form.BankListDst.length;k++){			
-				if(form.BankListDst.options[k].text == oOption.text){		    
-			   	   checkAdd = true;			       
-		    	}   
-	    	}
-	    	if(checkAdd == false && oOption.text != '' && oOption.value != ''){	  
-  				form.BankListSrc.add(oOption); 
-  			}	
-	    }else if (nodeType.item(i).firstChild.nodeValue == form.HSIEN_ID.value){
-  			oOption = document.createElement("OPTION");
+				if($(this).find("bntype").text() != '2'){
+					oOption.text= $(this).find("bankname").text();
+					oOption.value=$(this).find("bankvalue").text();
+				}
+			}else{//已裁撤
+				if($(this).find("bntype").text() == '2'){
+					oOption.text= $(this).find("bankname").text();
+					oOption.value=$(this).find("bankvalue").text();
+				}
+			}
+			checkAdd=false;
+			for(var k =0;k<form.BankListDst.length;k++){
+				if(form.BankListDst.options[k].text == oOption.text){
+					checkAdd = true;
+				}
+			}
+			if(checkAdd == false && oOption.text != '' && oOption.value != ''){
+				form.BankListSrc.add(oOption);
+			}
+		}else if ($(this).find("hsienid").text() == form.HSIEN_ID.value){
+			oOption = document.createElement("OPTION");
 			if(form.CANCEL_NO.value == 'N'){//營運中
-			   if(BnType.item(i).firstChild.nodeValue != '2'){			   	  
-			      oOption.text=nodeName.item(i).firstChild.nodeValue;
-  			      oOption.value=nodeValue.item(i).firstChild.nodeValue;   	  
-			   }		
-		    }else{//已裁撤
-		       if(BnType.item(i).firstChild.nodeValue == '2'){
-			      oOption.text=nodeName.item(i).firstChild.nodeValue;
-  			      oOption.value=nodeValue.item(i).firstChild.nodeValue;   	  
-			   }
-		    }	
-  			checkAdd=false;
-			for(var k =0;k<form.BankListDst.length;k++){			
-				if(form.BankListDst.options[k].text == oOption.text){		    
-			   	   checkAdd = true;			       
-		    	}   
-	    	}
-	    	if(checkAdd == false && oOption.text != '' && oOption.value != ''){	       
-  				form.BankListSrc.add(oOption); 
-  			}  			
-    	}
-    }
+				if($(this).find("bntype").text() != '2'){
+					oOption.text= $(this).find("bankname").text();
+					oOption.value=$(this).find("bankvalue").text();
+				}
+			}else{//已裁撤
+				if($(this).find("bntype").text() == '2'){
+					oOption.text= $(this).find("bankname").text();
+					oOption.value=$(this).find("bankvalue").text();
+				}
+			}
+			checkAdd=false;
+			for(var k =0;k<form.BankListDst.length;k++){
+				if(form.BankListDst.options[k].text == oOption.text){
+					checkAdd = true;
+				}
+			}
+			if(checkAdd == false && oOption.text != '' && oOption.value != ''){
+				form.BankListSrc.add(oOption);
+			}
+		}
+	})
+	;
 }
 function setSelect(S1, bankid) {
     if(S1 == null)
